a5adbb71641517e851c1b942e42881ba9fdcdbff
KavindiPathiraja <155365777+KavindiPathiraja@users.noreply.github.com>
2024-05-11 13:45:33 +0530

    date

--- app/src/main/java/com/example/taskmanagementapp/AddNoteActivity.kt
+++ app/src/main/java/com/example/taskmanagementapp/AddNoteActivity.kt
@@ -1,15 +1,24 @@
 package com.example.taskmanagementapp
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.taskmanagementapp.databinding.ActivityAddNoteBinding
-import com.example.taskmanagementapp.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+private lateinit var textDate: TextView
+private lateinit var buttonDate: Button
 
 class AddNoteActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddNoteBinding
     private lateinit var db: NoteDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddNoteBinding.inflate(layoutInflater)
@@ -20,11 +29,37 @@ class AddNoteActivity : AppCompatActivity() {
         binding.saveButton.setOnClickListener {
             val title = binding.titleEditText.text.toString()
             val content = binding.contentEditText.text.toString()
-            val note = Note(0,title,content)
+            val note = Note(0, title, content)
             db.insertNote(note)
             finish()
-            Toast.makeText(this,"Note Saved",Toast.LENGTH_SHORT).show()
-
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
         }
+
+        textDate = findViewById(R.id.textDate)
+        buttonDate = findViewById(R.id.buttonDate)
+
+        val calendarBox = Calendar.getInstance()
+        val dateBox = DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+            calendarBox.set(Calendar.YEAR, year)
+            calendarBox.set(Calendar.MONTH, month)
+            calendarBox.set(Calendar.DAY_OF_MONTH, day)
+
+            updateText(calendarBox)
+        }
+        buttonDate.setOnClickListener {
+            DatePickerDialog(
+                this,
+                dateBox,
+                calendarBox.get(Calendar.YEAR),
+                calendarBox.get(Calendar.MONTH),
+                calendarBox.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+    }
+
+    private fun updateText(calendar: Calendar) {
+        val dateFormat = "dd-MM-yyyy"
+        val simple = SimpleDateFormat(dateFormat, Locale.UK)
+        textDate.text = simple.format(calendar.time)
     }
 }
